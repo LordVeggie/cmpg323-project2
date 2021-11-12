440ce4eac8d2479017a8d638f3bf99e9b63b0b19
LordVeggie <35328638+LordVeggie@users.noreply.github.com>
2021-11-12 21:52:31 +0200

    gallery image and metta data java done

--- code/java/gallery.js
+++ code/java/gallery.js
@@ -1,6 +1,33 @@
 //this is where i will add the denamic content ofthe page to 
 const bodyOfGallery = document.querySelector('#bodyOfGallery');
 
+function creatTextBox(text, name)
+{
+    //create
+
+    const container = document.createElement('div');
+    container.classList.add('flex-center-column');
+    container.classList.add('textbox-container');
+
+    const labelText = document.createElement('label');
+    labelText.innerText = text;
+    labelText.for = name;
+
+    const textBox = document.createElement('input');
+    textBox.classList.add('textbox');
+    textBox.type = 'text';
+    textBox.name = name;
+    textBox.id = name;
+
+    //append
+    container.appendChild(labelText);
+    container.appendChild(textBox);
+
+    //return
+    return container;
+}
+
+
 //singal images 
 function creatImage(image)
 {
@@ -46,82 +73,13 @@ function layoutAllImages()
     bodyOfGallery.appendChild(imageGroup);
 
     return imageGroup;
-
 }
 
-
-<div class="">//
-
-    <div class=" title ">//
-        <label for="title"> some image </label>//
-    </div>
-    
-    <div class="imageWithMetta flex-row">
-        <!-- images and maettadata -->
-        <div class=" imageGroup ">
-    
-            <div class="image"></div>
-        </div>
-    
-        <div class="Metta flex-center-column">
-    
-            <div class=" TopSpace ">
-                <!--blanck space-->
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be first name field-->
-                <label for="Name">Name</label>
-                <input type="text" name="Name" id="Name" required class="textbox">
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be last name field-->
-                <label for="lname">Last name</label>
-                <input type="text" name="lname" id="lname" required class="textbox">
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be user name field-->
-                <label for="uname">User name</label>
-                <input type="text" name="uname" id="uname" required class="textbox">
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be password fiels-->
-                <label for="pass">Password</label>
-                <input type="password" name="pass" id="pass" required class="textbox">
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be confirm password fiels-->
-                <label for="passCon">Confirm Password</label>
-                <input type="password" name="passCon" id="passCon" required class="textbox">
-            </div>
-    
-            <div class="flex-center-column textbox-container">
-                <!--this will be used for error messages-->
-            </div>
-    
-            <div class="flex-center-column">
-                <!--this will be submit button-->
-                <!--if inpout is correct and validated navigate user to home page else error meassage-->
-                <button class="btn">Update</button>
-                <button class="btn">Delete</button>
-            </div>
-    
-        </div>
-    
-    </div>
-
-</div>
-
-
-function layoutImageAndMettaData(name)
+function layoutImageAndMettaData(name, image)
 {
     //changes the layout of the page to show one image and its mettadata
 
-    //ceate all elements 
+    //create all elements 
 
     const container = document.createElement('div');
 
@@ -132,11 +90,71 @@ function layoutImageAndMettaData(name)
     titleLabel.innerText = name;
 
     const imageWithMetta = document.createElement('div');
+    imageWithMetta.classList.add('imageWithMetta');
+    imageWithMetta.classList.add('flex-row');
 
+    const ImageGroup = document.createElement('div');
+    ImageGroup.classList.add('imageGroup');
+    const Image = creatImage(image);
 
+    const mettadata = document.createElement('div');
+    mettadata.classList.add('Metta');
+    mettadata.classList.add('flex-center-column');
+    
+    const topspace = document.createElement('div');
+    topspace.classList.add('TopSpace');
+
+    //creat all the metta data fields
+    const nameTextBox = creatTextBox('Name', 'Name');
+    const geoLocationTextBox = creatTextBox('GeoLocation', 'GeoLocation');
+    const captuerByTextBox = creatTextBox('Captuered By', 'CaptuerBy');
+    const captureDateTextBox = creatTextBox('Captuered Date', 'CaptureDate');
+
+    //gotta do some thing for tags ???
+    const tagTextBox = creatTextBox('Tag', 'Tag');
+
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('flex-center-column');
+    errorMessage.classList.add('textbox-container');
+
+    //buttons
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('flex-center-column');
+
+    const updateButton = document.createElement('button');
+    updateButton.innerText = 'Update';
+    updateButton.classList.add('btn');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.classList.add('btn');
+
+    buttonContainer.appendChild(updateButton);
+    buttonContainer.appendChild(deleteButton);
 
     //append all elements
 
+    title.appendChild(titleLabel);
+
+    ImageGroup.appendChild(Image);
+
+    mettadata.appendChild(topspace);
+    mettadata.appendChild(nameTextBox);
+    mettadata.appendChild(geoLocationTextBox);
+    mettadata.appendChild(captuerByTextBox);
+    mettadata.appendChild(captureDateTextBox);
+    mettadata.appendChild(tagTextBox);
+    mettadata.appendChild(errorMessage);
+
+    mettadata.appendChild(buttonContainer); 
+
+    imageWithMetta.appendChild(ImageGroup);
+    imageWithMetta.appendChild(mettadata);
+
+    container.appendChild(title);
+    container.appendChild(imageWithMetta);
+
+    bodyOfGallery.appendChild(container);
 }
 
 function layoutAlbumsAndImages()
@@ -147,7 +165,7 @@ function layoutAlbumsAndImages()
 }
 
 //tests of stuff
-if (0)
+if (1)
 {
     layoutAllImages().appendChild(creatImage('https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png'));
     const imageGroup = document.querySelector('#imageGroup');
@@ -156,4 +174,12 @@ if (0)
     {
         imageGroup.appendChild(creatImage('https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png'));
     }    
+
+    imageGroup.appendChild(creatTextBox('test test', 'test'));
+
+    imageGroup.appendChild(creatTextBox('test test', 'test2'));
+
+    imageGroup.appendChild(creatTextBox('test test', 'test3'));
+
+    layoutImageAndMettaData('some random logo', 'https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png');
 }
