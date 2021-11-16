5619dc9a46f9512304a52a2f5daa0109e9d7720e
LordVeggie <35328638+LordVeggie@users.noreply.github.com>
2021-11-16 23:40:34 +0200

    back end

--- code/node/app/index.js
+++ code/node/app/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static( path.join(__dirname,'/../../css')));
+app.use(express.static( path.join(__dirname,'/../../images_of_pages')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'/views'));
@@ -13,6 +14,35 @@ app.get('/',(req, res) =>
     res.render('login');
 })
 
+app.get('/signup',(req, res) =>
+{
+    res.render('signup');
+})
+
+app.get('/shared',(req, res) =>
+{
+    res.render('shared');
+})
+
+app.get('/account',(req, res) =>
+{
+    res.render('account');
+})
+
+app.get('/friends',(req, res) =>
+{
+    res.render('friends');
+})
+
+app.get('/gallery',(req, res) =>
+{
+    res.render('gallery');
+})
+
+app.get('/home',(req, res) =>
+{
+    res.render('home');
+})
 
 app.listen(3000, ()=>
 {
